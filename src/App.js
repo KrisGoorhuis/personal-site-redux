4675f717eac272bdeae03b334a97b194fbf554a2
krisgoorhuis <krisgoorhuis@gmail.com>
2019-08-19 19:33:01 -0700

    Testing deployment pipeline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header';
+
+import Header from 'Components/Header/header.js';
 
 function App() {
   return (
---
+++ src/Components/More/more.js
@@ -0,0 +1,4 @@
+// Links to resources that helped along the way.
+// freecodecamp
+// The Coding Train https://www.youtube.com/channel/UCvjgXvBlbQiydffZU7m1_aw
+// Do something with Unity. Hobby stuff!
